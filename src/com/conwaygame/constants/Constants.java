47a5693a3aaf89dfa780586eb0a82d4e69f411c5
mohak92 <tamhane.mohak@gmail.com>
2020-05-01 22:57:45 -0400

    Constants for menu options, rows and columns

--- src/com/conwaygame/constants/Constants.java
+++ src/com/conwaygame/constants/Constants.java
@@ -1,5 +1,27 @@
 package com.conwaygame.constants;
 
 public class Constants {
+	
+	private Constants() {
+		
+	}
+	
+	public static final String NAME_OF_APPLICATION = "Conway's Game";
+	public static final int NUMBER_OF_COLUMNS = 30;
+	public static final int NUMBER_OF_ROWS = 30;
+	public static final String START = "Start";
+	public static final String RESTART = "Restart";
+	public static final int TIME_LAG = 200;
+	
+	public static final int FRAME_WIDTH = 700;
+	public static final int FRAME_HEIGHT = 700;
+	
+	public static final String GREEN_COLOR = "#01a900";
+	
+	public static final String MENU_FILE = "File";
+	public static final String MENU_EXIT = "Exit";
+	public static final String MENU_ABOUT =  "About";
+	public static final String MENU_EXIT_TEXT = "Do you want to exit ?";
+	public static final String MENU_EXIT_TITLE = "Warning";
 
 }
