75d6c71a6cfb4503adb3fbd7ade906ee35a448c6
mohak92 <tamhane.mohak@gmail.com>
2020-05-19 22:44:07 -0400

    Main class

--- src/com/conwaygame/app/App.java
+++ src/com/conwaygame/app/App.java
@@ -1,9 +1,27 @@
 package com.conwaygame.app;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new MainFrame();
+			}
+		});
 	}
 
 }
