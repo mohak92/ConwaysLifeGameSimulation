9b3e607bbef5cdbe3ce9b4af3f7b14263c47dae8
mohak92 <tamhane.mohak@gmail.com>
2020-05-31 14:14:26 -0400

    counter time panel

--- src/com/conwaygame/gui/TimePanel.java
+++ src/com/conwaygame/gui/TimePanel.java
@@ -1,5 +1,33 @@
 package com.conwaygame.gui;
 
-public class TimePanel {
+import java.awt.FlowLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TimePanel extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+	private JLabel timeLabel;
+	public int count = 0;
+	
+	public TimePanel() {
+		initializeLayout();
+	}
+
+	private void initializeLayout() {
+		setLayout(new FlowLayout(FlowLayout.CENTER));
+		timeLabel = new JLabel("Generations: 0");
+		add(timeLabel);
+	}
+	
+	public void refreshCounter() {
+		count = 0;
+		refresh();
+	}
+	
+	public void refresh() {
+		timeLabel.setText("Generations: "+count++);
+	}
 
 }
