168eedcddaf3d34689a96e49c4d8c9354142d2bf
mohak92 <tamhane.mohak@gmail.com>
2020-05-31 14:13:56 -0400

    controller start and stop thread

--- src/com/conwaygame/gui/Controller.java
+++ src/com/conwaygame/gui/Controller.java
@@ -1,5 +1,37 @@
 package com.conwaygame.gui;
 
-public class Controller {
+import com.conwaygame.constants.Constants;
 
+public class Controller implements Runnable {
+	
+	private Board board;
+	private volatile static boolean keepGoing = true;
+
+	public Controller(Board board) {
+		this.board = board;
+	}
+	
+	@Override
+	public void run() {
+		while(!Thread.currentThread().isInterrupted() && keepGoing) {
+			
+			board.newIteration();
+			
+			try {
+				Thread.sleep(Constants.TIME_LAG);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+	}
+
+	public static void startThread() {
+		keepGoing = true;
+	}
+
+	public static void stopThread() {
+		Thread.currentThread().interrupt();
+		keepGoing = false;
+	}
 }
